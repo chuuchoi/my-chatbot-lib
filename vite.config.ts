d1a26e3d307a28a3f66d9b63e07ea3266cba18c9
chuuchoi <51234226+chuuchoi@users.noreply.github.com>
2025-07-24 10:26:04 +0900

    lib name modify

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/Chatbot.tsx'),
       name: 'MyChatbotLib',
-      fileName: (format) => `my-chatbot-lib.${format}.js`,
+      fileName: (format) => `my-chatbot-lib-0.${format}.js`,
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
